0117345c004821e8899990c1bec5a14be456b23e
to4to <justjustifying@outlook.com>
2024-06-25 15:49:40 +0530

    Owner Modifier Defined

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -2,17 +2,25 @@
 
 
 pragma solidity ^0.8.20;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 
 contract FundMe{
 
 
+    error FundMe_NotOwner();
+
+  address  private immutable  i_owner;
+
+
+    modifier onlyOwner(){
+
+        if(msg.sender!==i_owner) revert FundMe_NotOwner();
+        _;
+
+    }
     constructor(){
 
     }
-
-
-
-
 
 }
