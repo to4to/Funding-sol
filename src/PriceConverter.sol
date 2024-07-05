d4fca0d95df56e67fca75b786127ba794b498929
to4to <justjustifying@outlook.com>
2024-07-05 11:31:07 +0530

    fix: Correct pragma statement in PriceConverter.sol

--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -1,11 +1,11 @@
 
 
 //SPDX-License-Identifier: MIT
-pragma ^0.8.20
+pragma  solidity ^0.8.20;
 
 
 
-impoer {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 
 library PriceConverter{
