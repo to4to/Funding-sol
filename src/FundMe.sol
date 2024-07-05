24712939f4c9dd9d124211410817aa2c07945066
to4to <justjustifying@outlook.com>
2024-07-05 11:30:03 +0530

    feat: Add PriceConverter library for price conversion

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,27 +1,31 @@
 //SPDX-Licence-Identifier: MIT
 
-
 pragma solidity ^0.8.20;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-
-contract FundMe{
-
-
+contract FundMe {
     error FundMe_NotOwner();
 
-  address  private immutable  i_owner;
-AggregatorV3Interface private s_priceFeed;
-address private imutable i_owner;
-    modifier onlyOwner(){
+    address private immutable i_owner;
+    AggregatorV3Interface private s_priceFeed;
 
-        if(msg.sender!==i_owner) revert FundMe_NotOwner();
+    modifier onlyOwner() {
+        //require(msg.sender==i_owner,"You Are not owner")
+        if (msg.sender != i_owner) revert FundMe_NotOwner();
         _;
-
-    }
-    constructor(address priceFeed){
-s_priceFeed= AggregatorV3Interface(priceFeed);
-i_owner=msg.sender;
     }
 
+    constructor(address priceFeed) {
+        s_priceFeed = AggregatorV3Interface(priceFeed);
+        i_owner = msg.sender;
+    }
+
+
+
+
+
+    function fund() public payable{
+
+        
+    }
 }
---
+++ src/PriceConverter.sol
@@ -0,0 +1,22 @@
+
+
+//SPDX-License-Identifier: MIT
+pragma ^0.8.20
+
+
+
+impoer {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+
+library PriceConverter{
+
+
+
+
+function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256){
+
+}
+
+
+
+}
