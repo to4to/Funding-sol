a17e2d03e2a4b29818e34c485552ef5b57ecedba
to4to <justjustifying@outlook.com>
2024-07-14 23:05:16 +0530

    feat getPrice Function

--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -1,22 +1,13 @@
-
-
 //SPDX-License-Identifier: MIT
-pragma  solidity ^0.8.20;
-
-
+pragma solidity ^0.8.20;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-
-library PriceConverter{
-
-
-
-
-function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256){
-
-}
-
-
-
+library PriceConverter {
+    function getPrice(
+        AggregatorV3Interface priceFeed
+    ) internal view returns (uint256) {
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        return uint256(answer * 10000000000);
+    }
 }
