ced4f9a4aa30e40bf0bc154f29afce39ac0f1c2a
to4to <justjustifying@outlook.com>
2024-07-14 23:10:03 +0530

    feat: getConversionRate

--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -10,4 +10,13 @@ library PriceConverter {
         (, int256 answer, , , ) = priceFeed.latestRoundData();
         return uint256(answer * 10000000000);
     }
+
+    function getConversionRate(
+        uint256 ethAmount,
+        AggregatorV3Interface priceFeed
+    ) internal view returns (uint256) {
+        uint256 ethPrice = getPrice(priceFeed);
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        return ethAmountInUsd;
+    }
 }
