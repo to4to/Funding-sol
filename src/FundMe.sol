4410c7d41e2d8c05f99ee864f681ad669a185185
to4to <justjustifying@outlook.com>
2024-10-26 23:03:21 +0530

    refactor: Update Solidity pragma statements to version 0.8.26

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,6 +1,6 @@
 //SPDX-Licence-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.26;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
